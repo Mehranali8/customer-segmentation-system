6903e12026ffa7392f95905817d4db763b7c5ab7
Mehran Ali <aliawan8694@gmail.com>
2026-07-15 14:08:09 -0700

    Fix CORS for Vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,6 +30,10 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+
+        "https://customer-segmentation-system-gamma.vercel.app",
+        "https://customer-segmentation-system-qwpil9vby.vercel.app",
+        "https://customer-segmentation-system-3msswgm0o.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
